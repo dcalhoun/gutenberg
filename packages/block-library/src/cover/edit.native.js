7a18bbe2ab909a538ef5ce466e2fdadffaccbef8
shadow351 <>
2020-10-15 06:41:51 -0500

    Ensure focal point coordinates are never empty
    Avoid error thrown from referencing empty focal point.

--- packages/block-library/src/cover/edit.native.js
+++ packages/block-library/src/cover/edit.native.js
@@ -295,6 +295,13 @@ const Cover = ( {
 		</TouchableWithoutFeedback>
 	);
 
+	function focalPointPosition( { x, y } = IMAGE_DEFAULT_FOCAL_POINT ) {
+		return {
+			left: `${ ( hasParallax ? 0.5 : x ) * 100 }%`,
+			top: `${ ( hasParallax ? 0.5 : y ) * 100 }%`,
+		};
+	}
+
 	const controls = (
 		<InspectorControls>
 			<OverlayColorSettings
@@ -359,18 +366,7 @@ const Cover = ( {
 									testMe={ true }
 									style={ [
 										styles.focalPointHint,
-										{
-											left: `${
-												( hasParallax
-													? 0.5
-													: focalPoint.x ) * 100
-											}%`,
-											top: `${
-												( hasParallax
-													? 0.5
-													: focalPoint.y ) * 100
-											}%`,
-										},
+										focalPointPosition( focalPoint ),
 									] }
 								/>
 							</View>
