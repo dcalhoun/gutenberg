71bb60b8251b444c86f884884d5275148b62a820
shadow351 <>
2020-10-17 11:39:05 -0500

    [wip] Attempt to position tooltip

--- packages/components/src/focal-point-picker/tooltip/index.native.js
+++ packages/components/src/focal-point-picker/tooltip/index.native.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import React from 'react';
 import {
 	Animated,
 	Easing,
@@ -14,24 +15,47 @@ import {
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useEffect, useRef, useState } from '@wordpress/element';
+import { useEffect, useRef, useState, useContext } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import styles from './style.scss';
 
-const Tooltip = ( { onTooltipHidden } ) => {
-	const [ visible, setVisible ] = useState( true );
-	const animationValue = useRef( new Animated.Value( 0 ) ).current;
+const TooltipContext = React.createContext( { visible: true } );
 
-	useEffect( () => {
-		startAnimation();
-	}, [ visible ] );
+function TooltipOverlay( { children } ) {
+	const [ visible, setVisible ] = useState( true );
 
 	const onHide = () => {
 		setVisible( false );
 	};
+
+	const stylesOverlay = [
+		styles.overlay,
+		{ height: Dimensions.get( 'window' ).height },
+	];
+
+	return (
+		<TooltipContext.Provider value={ { visible } }>
+			{ children }
+			{ false && visible && (
+				<TouchableWithoutFeedback onPress={ onHide }>
+					<View style={ stylesOverlay } />
+				</TouchableWithoutFeedback>
+			) }
+		</TooltipContext.Provider>
+	);
+}
+
+const Tooltip = ( { onTooltipHidden, additionalOffset = { x: 0, y: 0 } } ) => {
+	const animationValue = useRef( new Animated.Value( 0 ) ).current;
+	const { visible } = useContext( TooltipContext );
+	const [ dimensions, setDimensions ] = useState( null );
+
+	useEffect( () => {
+		startAnimation();
+	}, [ visible ] );
 
 	const startAnimation = () => {
 		Animated.timing( animationValue, {
@@ -47,54 +71,68 @@ const Tooltip = ( { onTooltipHidden } ) => {
 		} );
 	};
 
-	const stylesOverlay = [
-		styles.overlay,
-		{ height: Dimensions.get( 'window' ).height },
-	];
+	let tooltipTransforms;
+	if ( dimensions ) {
+		tooltipTransforms = [
+			{ translateX: -dimensions.width / 2 + ( additionalOffset.x || 0 ) },
+			{ translateY: -dimensions.height + ( additionalOffset.y || 0 ) },
+		];
+	}
 
 	return (
-		<>
-			<TouchableWithoutFeedback onPress={ onHide }>
-				<View style={ stylesOverlay } />
-			</TouchableWithoutFeedback>
-			<Animated.View
-				style={ {
-					opacity: animationValue,
-					transform: [
+		<Animated.View
+			style={ {
+				opacity: animationValue,
+				transform: [
+					{
+						translateY: animationValue.interpolate( {
+							inputRange: [ 0, 1 ],
+							outputRange: [ visible ? 0 : -8, 0 ],
+						} ),
+					},
+				],
+			} }
+		>
+			<TouchableWithoutFeedback>
+				{ /* <View style={ styles.tooltipWrap }> */ }
+				<View
+					onLayout={ ( { nativeEvent } ) => {
+						const { height, width } = nativeEvent.layout;
+						setDimensions( { height, width } );
+					} }
+					style={ [
+						styles.tooltip,
 						{
-							translateY: animationValue.interpolate( {
-								inputRange: [ 0, 1 ],
-								outputRange: [ visible ? 4 : -8, -8 ],
-							} ),
+							shadowColor: styles.tooltipShadow.color,
+							shadowOffset: {
+								width: 0,
+								height: 2,
+							},
+							shadowOpacity: 0.25,
+							shadowRadius: 2,
+							elevation: 2,
+							transform: tooltipTransforms,
 						},
-					],
-				} }
-			>
-				<TouchableWithoutFeedback onPress={ onHide }>
+					] }
+				>
+					<Text style={ styles.text }>
+						{ __( 'Drag to adjust focal point' ) }
+					</Text>
 					<View
 						style={ [
-							styles.tooltip,
+							styles.arrow,
 							{
-								shadowColor: styles.tooltipShadow.color,
-								shadowOffset: {
-									width: 0,
-									height: 2,
-								},
-								shadowOpacity: 0.25,
-								shadowRadius: 2,
-								elevation: 2,
+								// transform: [ { translateX: -6 } ],
 							},
 						] }
-					>
-						<Text style={ styles.text }>
-							{ __( 'Try a starter layout' ) }
-						</Text>
-						<View style={ styles.arrow } />
-					</View>
-				</TouchableWithoutFeedback>
-			</Animated.View>
-		</>
+					/>
+				</View>
+				{ /* </View> */ }
+			</TouchableWithoutFeedback>
+		</Animated.View>
 	);
 };
+
+Tooltip.Overlay = TooltipOverlay;
 
 export default Tooltip;
