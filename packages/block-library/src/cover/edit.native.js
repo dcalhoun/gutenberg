0303043f041f96ea19c0cf3d23ca1c2a88dc6e17
shadow351 <>
2020-10-27 20:59:52 -0500

    Abstract Cover controls to reduce file size
    Relocate the controls sections into a new file to reduce the size of the
    Cover edit file.

--- packages/block-library/src/cover/edit.native.js
+++ packages/block-library/src/cover/edit.native.js
@@ -19,10 +19,6 @@ import {
 	Image,
 	ImageEditingButton,
 	IMAGE_DEFAULT_FOCAL_POINT,
-	PanelBody,
-	RangeControl,
-	BottomSheet,
-	ToggleControl,
 	ToolbarButton,
 	ToolbarGroup,
 	Gradient,
@@ -46,7 +42,7 @@ import {
 import { compose, withPreferredColorScheme } from '@wordpress/compose';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { useEffect, useState, useRef } from '@wordpress/element';
-import { cover as icon, plus, replace, image, warning } from '@wordpress/icons';
+import { cover as icon, replace, image, warning } from '@wordpress/icons';
 import { getProtocol } from '@wordpress/url';
 
 /**
@@ -55,12 +51,11 @@ import { getProtocol } from '@wordpress/url';
 import styles from './style.scss';
 import {
 	attributesFromMedia,
-	COVER_MIN_HEIGHT,
 	IMAGE_BACKGROUND_TYPE,
 	VIDEO_BACKGROUND_TYPE,
+	COVER_DEFAULT_HEIGHT,
 } from './shared';
-import OverlayColorSettings from './overlay-color-settings';
-import FocalPointSettings from './focal-point-settings';
+import Controls from './controls';
 
 /**
  * Constants
@@ -75,8 +70,6 @@ const INNER_BLOCKS_TEMPLATE = [
 		},
 	],
 ];
-const COVER_MAX_HEIGHT = 1000;
-const COVER_DEFAULT_HEIGHT = 300;
 
 const Cover = ( {
 	attributes,
@@ -92,7 +85,6 @@ const Cover = ( {
 		backgroundType,
 		dimRatio,
 		focalPoint,
-		hasParallax,
 		minHeight,
 		url,
 		id,
@@ -161,16 +153,6 @@ const Cover = ( {
 		onSelect( media );
 	};
 
-	const onHeightChange = ( value ) => {
-		if ( minHeight || value !== COVER_DEFAULT_HEIGHT ) {
-			setAttributes( { minHeight: value } );
-		}
-	};
-
-	const onOpactiyChange = ( value ) => {
-		setAttributes( { dimRatio: value } );
-	};
-
 	const onMediaPressed = () => {
 		if ( isUploadInProgress ) {
 			requestImageUploadCancelDialog( id );
@@ -211,21 +193,6 @@ const Cover = ( {
 		} );
 	}
 
-	const [ displayPlaceholder, setDisplayPlaceholder ] = useState( true );
-
-	function setFocalPoint( value ) {
-		setAttributes( { focalPoint: value } );
-	}
-
-	const toggleParallax = () => {
-		setAttributes( {
-			hasParallax: ! hasParallax,
-			...( ! hasParallax
-				? { focalPoint: undefined }
-				: { focalPoint: IMAGE_DEFAULT_FOCAL_POINT } ),
-		} );
-	};
-
 	function openColorPicker() {
 		if ( isParentSelected ) {
 			setCustomColorPickerShowing( true );
@@ -236,11 +203,6 @@ const Cover = ( {
 	const backgroundColor = getStylesFromColorScheme(
 		styles.backgroundSolid,
 		styles.backgroundSolidDark
-	);
-
-	const addMediaButtonStyle = getStylesFromColorScheme(
-		styles.addMediaButton,
-		styles.addMediaButtonDark
 	);
 
 	const overlayStyles = [
@@ -293,158 +255,6 @@ const Cover = ( {
 				</View>
 			</View>
 		</TouchableWithoutFeedback>
-	);
-
-	function focalPointPosition( { x, y } = IMAGE_DEFAULT_FOCAL_POINT ) {
-		return {
-			left: `${ ( hasParallax ? 0.5 : x ) * 100 }%`,
-			top: `${ ( hasParallax ? 0.5 : y ) * 100 }%`,
-		};
-	}
-
-	const [ videoNaturalSize, setVideoNaturalSize ] = useState( null );
-
-	const imagePreviewStyles = [
-		displayPlaceholder && styles.imagePlaceholder,
-	];
-	const videoPreviewStyles = [
-		{
-			aspectRatio:
-				videoNaturalSize &&
-				videoNaturalSize.width / videoNaturalSize.height,
-			height: '100%',
-		},
-		! displayPlaceholder && styles.video,
-		displayPlaceholder && styles.imagePlaceholder,
-	];
-
-	const controls = (
-		<InspectorControls>
-			<OverlayColorSettings
-				attributes={ attributes }
-				setAttributes={ setAttributes }
-			/>
-			{ url ? (
-				<PanelBody>
-					<RangeControl
-						label={ __( 'Opacity' ) }
-						minimumValue={ 0 }
-						maximumValue={ 100 }
-						value={ dimRatio }
-						onChange={ onOpactiyChange }
-						style={ styles.rangeCellContainer }
-						separatorType={ 'topFullWidth' }
-					/>
-				</PanelBody>
-			) : null }
-			<PanelBody title={ __( 'Dimensions' ) }>
-				<RangeControl
-					label={ __( 'Minimum height in pixels' ) }
-					minimumValue={ COVER_MIN_HEIGHT }
-					maximumValue={ COVER_MAX_HEIGHT }
-					value={ CONTAINER_HEIGHT }
-					onChange={ onHeightChange }
-					style={ styles.rangeCellContainer }
-				/>
-			</PanelBody>
-
-			<PanelBody title={ __( 'Media' ) }>
-				{ url ? (
-					<>
-						<BottomSheet.Cell
-							style={ backgroundColor }
-							cellContainerStyle={ styles.mediaPreview }
-						>
-							<View style={ styles.mediaInner }>
-								{ IMAGE_BACKGROUND_TYPE === backgroundType && (
-									<Image
-										editButton={ ! displayPlaceholder }
-										isSelected={ ! displayPlaceholder }
-										isUploadFailed={ didUploadFail }
-										isUploadInProgress={
-											isUploadInProgress
-										}
-										onImageDataLoad={ () => {
-											setDisplayPlaceholder( false );
-										} }
-										onSelectMediaUploadOption={
-											onSelectMedia
-										}
-										openMediaOptions={
-											openMediaOptionsRef.current
-										}
-										url={ url }
-										height="100%"
-										style={ imagePreviewStyles }
-										width={ styles.image.width }
-									/>
-								) }
-								{ VIDEO_BACKGROUND_TYPE === backgroundType && (
-									<Video
-										muted
-										paused
-										disableFocus
-										onLoad={ ( event ) => {
-											const {
-												height,
-												width,
-											} = event.naturalSize;
-											setVideoNaturalSize( {
-												height,
-												width,
-											} );
-											setDisplayPlaceholder( false );
-										} }
-										resizeMode={ 'cover' }
-										source={ { uri: url } }
-										style={ videoPreviewStyles }
-									/>
-								) }
-								{ ! hasParallax && ! displayPlaceholder && (
-									<Icon
-										icon={ plus }
-										size={ styles.focalPointHint.width }
-										testMe={ true }
-										style={ [
-											styles.focalPointHint,
-											focalPointPosition( focalPoint ),
-										] }
-									/>
-								) }
-							</View>
-						</BottomSheet.Cell>
-						<FocalPointSettings
-							focalPoint={
-								focalPoint || IMAGE_DEFAULT_FOCAL_POINT
-							}
-							mediaType={ backgroundType }
-							onFocalPointChange={ setFocalPoint }
-							url={ url }
-						/>
-						{ IMAGE_BACKGROUND_TYPE === backgroundType && (
-							<ToggleControl
-								label={ __( 'Fixed background' ) }
-								checked={ hasParallax }
-								onChange={ toggleParallax }
-							/>
-						) }
-						<BottomSheet.Cell
-							leftAlign
-							label={ __( 'Clear Media' ) }
-							labelStyle={ styles.clearMediaButton }
-							onPress={ onClearMedia }
-						/>
-					</>
-				) : (
-					<BottomSheet.Cell
-						label={ __( 'Add image or video' ) }
-						labelStyle={ addMediaButtonStyle }
-						leftAlign
-						onPress={ openMediaOptionsRef.current }
-					/>
-				) }
-			</PanelBody>
-		</InspectorControls>
 	);
 
 	const colorPickerControls = (
@@ -620,7 +430,15 @@ const Cover = ( {
 
 	return (
 		<View style={ styles.backgroundContainer }>
-			{ controls }
+			<Controls
+				attributes={ attributes }
+				didUploadFail={ didUploadFail }
+				isUploadInProgress={ isUploadInProgress }
+				onClearMedia={ onClearMedia }
+				onSelectMedia={ onSelectMedia }
+				openMediaOptionsRef={ openMediaOptionsRef }
+				setAttributes={ setAttributes }
+			/>
 
 			{ isImage &&
 				url &&
